1bd97728409be7b8b04bbbf2d3f31fb6af3a766a
Alexander Popov <lxndrppv@gmail.com>
2015-09-14 21:43:30 +0300

    [+] initial commit

---
+++ Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent navAgent = null;
+
+    void InitializeComponents()
+    {
+        navAgent = this.GetComponent<NavMeshAgent>();
+        Debug.Assert(navAgent != null, "Nav mesh agent component not set for player movement class.");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        InitializeComponents();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForDirectionInput();
+    }
+
+    void CheckForDirectionInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(screenRay, out hit) == true)
+            {
+                navAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}

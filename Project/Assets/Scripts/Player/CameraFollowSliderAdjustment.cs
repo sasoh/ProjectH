75c4ac31d8065e1acc0138625023b2b1c16d31b5
Alexander Popov <lxndrppv@gmail.com>
2015-09-14 22:33:42 +0300

    [+] added slider to change camera to player distance

---
+++ Project/Assets/Scripts/Player/CameraFollowSliderAdjustment.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraFollowSliderAdjustment : MonoBehaviour
+{
+
+    public UnityStandardAssets.Utility.FollowTarget followTargetScript = null;
+    public float DistanceAmount = 10.0f;
+    public Text distanceText = null;
+    private Vector3 startingOffset;
+
+    void InitializeComponents()
+    {
+        Debug.Assert(followTargetScript != null, "Follow target script not set.");
+        Debug.Assert(distanceText != null, "Distance text object not set.");
+    }
+
+    void Start()
+    {
+        InitializeComponents();
+
+        startingOffset = followTargetScript.offset;
+        UpdateText();
+    }
+
+    public void DidChangeSliderValue(float value)
+    {
+        Vector3 currentOffset = startingOffset - followTargetScript.transform.forward * DistanceAmount * value;
+        followTargetScript.offset = currentOffset;
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        string newValue = "Y: " + followTargetScript.offset.y;
+        distanceText.text = newValue;
+    }
+}
